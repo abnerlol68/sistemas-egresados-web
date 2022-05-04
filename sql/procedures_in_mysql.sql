500f5b1bad29b9cf0640e707a7c92a399d77003b
Abner WSL <abnerstr81@gmail.com>
2022-05-04 15:23:01 -0500

    Modifications in progress for the structure for DB

--- sql/procedures_in_mysql.sql
+++ sql/procedures_in_mysql.sql
@@ -1,33 +1,33 @@
 CREATE PROCEDURE insert_graduate(
-    IN ip_g VARCHAR(16)
+    IN email_g VARCHAR(64)
 ) BEGIN
-    INSERT INTO graduates (ip) VALUES (ip_g);
+    INSERT INTO graduates (email) VALUES (email_g);
 END;
 -- CALL insert_graduate("192.168.0.24");
 
 CREATE PROCEDURE check_graduates(
-    IN ip_to_check VARCHAR(16)
+    IN email_to_check VARCHAR(64)
 )
 BEGIN
-    SELECT * FROM graduates WHERE ip = ip_to_check;
+    SELECT * FROM graduates WHERE email = email_to_check;
 END;
 -- CALL check_graduates("192.168.0.24")
 
 CREATE PROCEDURE get_id_graduate(
-    IN ip_g VARCHAR(16)
+    IN email_g VARCHAR(64)
 ) BEGIN
-    SELECT id FROM graduates WHERE ip = ip_g;
+    SELECT id FROM graduates WHERE email = email_g;
 END;
 -- CALL get_id_graduate("127.0.0.1");
 
 CREATE PROCEDURE get_id_implemented_question(
     IN id_form INT,
-    IN descip VARCHAR(255)
+    IN descript VARCHAR(255)
 ) BEGIN
     SELECT implemented_questions.id AS "implemented_questions.id" FROM implemented_questions
         INNER JOIN questions
     WHERE implemented_questions.fk_form = id_form
-        AND questions.description_q = descip
+        AND questions.descremailtion_q = descript
         AND implemented_questions.fk_question = questions.id;
 END;
 -- CALL get_id_implemented_question(1, "Nombre Completo");
@@ -35,9 +35,9 @@ END;
 CREATE PROCEDURE insert_answer(
     IN id_graduate INT,
     IN id_implemented_questions INT,
-    IN descrip VARCHAR(255)
+    IN descript VARCHAR(255)
 ) BEGIN
     INSERT INTO answers (fk_graduate, fk_implemented_questions, description_a)
-    VALUES (id_graduate, id_implemented_questions, descrip);
+    VALUES (id_graduate, id_implemented_questions, descript);
 END;
 -- CALL insert_answer(:id_graudate, :id_implemented_question, :answer);
--- sql/set_db.sql
+++ sql/set_db.sql
@@ -1,19 +1,25 @@
 DROP DATABASE IF EXISTS graduate_forms;
+
 CREATE DATABASE graduate_forms;
+
 USE graduate_forms;
+
 CREATE TABLE graduates (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    ip VARCHAR(16) NOT NULL
+    email VARCHAR(16) NOT NULL UNIQUE
 ) ENGINE=INNODB;
+
 CREATE TABLE forms (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(128) NOT NULL
 ) ENGINE=INNODB;
+
 CREATE TABLE questions (
     id INT AUTO_INCREMENT PRIMARY KEY,
     description_q VARCHAR(255) NOT NULL UNIQUE,
     type_q VARCHAR(12)
 ) ENGINE=INNODB;
+
 CREATE TABLE implemented_questions (
     id INT AUTO_INCREMENT PRIMARY KEY,
     fk_form INT,
@@ -21,6 +27,7 @@ CREATE TABLE implemented_questions (
     FOREIGN KEY (fk_form) REFERENCES forms(id) ON UPDATE CASCADE ON DELETE RESTRICT,
     FOREIGN KEY (fk_question) REFERENCES questions(id) ON UPDATE CASCADE ON DELETE RESTRICT
 ) ENGINE=INNODB;
+
 CREATE TABLE answers (
     id INT AUTO_INCREMENT PRIMARY KEY,
     fk_graduate INT,
