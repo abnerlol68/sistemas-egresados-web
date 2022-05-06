064e03f685132d47cfb8b6717fc21f8670cc1998
Abner Perez <abnerstr81@gmail.com>
2022-05-06 17:59:15 -0500

    Settings for DB connection from Heroku

--- Model/database.php
+++ Model/database.php
@@ -1,13 +1,19 @@
 <?php
 
-$host = "127.0.0.1";
+$host = "db-for-graduates-db00006452.mdb0002659.db1.skysql.net";
 $database = "graduate_forms";
-$user = "abner";
-$password = "myu53r4bn3rdb";
+$user = "DB00006452";
+$password = "Hell47Drax+";
+$port = "5001";
+$options = array(
+  PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+  PDO::MYSQL_ATTR_SSL_CA => '../aws_skysql_chain.pem',
+  PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+);
 
 try {
-  //  Conexión
-  $conn = new PDO("mysql:host=$host;dbname=$database", $user, $password);
+  $conn = new PDO("mysql:host=$host;port=$port;dbname=$database", $user, $password, $options);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
   die("PDO Connection Error: " . $e->getMessage());
 }
